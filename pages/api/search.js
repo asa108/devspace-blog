eb9728b50972ce1b4eb6f6f0372875ad1841578f
Asane <yaradayo317@gmail.com>
2021-12-10 17:01:39 -0500

    corrected spells

--- pages/api/search.js
+++ pages/api/search.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-export default (req, res) => {
+export default function handler(req, res) {
   let posts;
 
   if (process.env.NODE_ENV === "production") {
